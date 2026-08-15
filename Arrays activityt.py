dbba8745a472fba4b472f434e9c3230e939afc17
Adu69 <83687446+Adu69@users.noreply.github.com>
2026-08-15 22:54:41 +0530

    Add files via upload

---
+++ Arrays activityt.py
@@ -0,0 +1,44 @@
+# Part 1
+scores = [10, 20, 30, 40, 50]
+start, end = 0, len(scores) - 1
+while start < end:
+    scores[start], scores[end] = scores[end], scores[start]
+    start += 1
+    end -= 1
+print("Swapped scores:", scores)
+print()
+
+# Part 2
+scores = [1, 2, 3, 4, 5, 6, 7, 8]
+n, i = 3, 0
+while i < len(scores):
+    start, end = i, min(i + n -1, len(scores) - 1)
+    while start < end:
+        scores[start], scores[end] = scores[end], scores[start]
+        start += 1
+        end -= 1
+    i += n
+print("Scores after reversing every 3 elements:", scores)
+print()
+
+# Part 3
+scores = [10, 20, 30, 40, 50]
+for _ in range(2):
+    temp = scores[0]
+    for i in range(1, len(scores)):
+        scores[i - 1] = scores[i]
+    scores[-1] = temp
+print("Rotated left by 2:", scores)
+print()
+
+# Part 4
+scores = [16, 17, 4, 3, 5, 2,]
+max_right = scores[-1]
+leaders = [max_right]
+for i in range(len(scores) - 2, -1, -1):
+    if scores[i] > max_right:
+        leaders.append(scores[i])
+        max_right = scores[i]
+leaders.reverse()
+print("Scores:", scores)
+print("Leaders in the array:", leaders)
